72e5967e3e94d498b945102258744c73d250138b
tbaecker @pi <tbaecker.spam@gmail.com>
2019-09-06 15:48:26 +0200

    small adjustments

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 # set -e  Using failing grep...
-set -x
+set +x
 
 grep rasp-net-monitor /etc/rc.local
 if [[ $? == 1 ]]; then
+    echo "rasp-net-monitor not yet installed to /etc/rc.local for launch at startup..."
     if (( $EUID != 0 )); then
-        echo "Please run as root"
+        echo "Must be root to install into rc.local... Please run with:"
+        echo "sudo $0"
         exit
     fi
     sed -i -e '$i \/home/pi/rasp-net-monitor/rasp-net-monitor.py >> /tmp/rasp-net-monitor_stdout.log 2>&1 &\n' /etc/rc.local
+    echo "Installed into rc.local - sed returned $?"
 else
     echo "rasp-net-monitor appears to be already installed:"
     echo ""
