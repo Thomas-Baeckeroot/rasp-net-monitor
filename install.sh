f81e2d08641c1ef4ff94256f7cb4ae19d6763775
Thomas Baeckeroot <tbaeckeroot@sophiagenetics.com>
2019-09-06 15:39:06 +0200

    Install script and others.

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
-set -e
+# set -e  Using failing grep...
 set -x
 
+grep rasp-net-monitor /etc/rc.local
+if [[ $? == 1 ]]; then
+    if (( $EUID != 0 )); then
+        echo "Please run as root"
+        exit
+    fi
+    sed -i -e '$i \/home/pi/rasp-net-monitor/rasp-net-monitor.py >> /tmp/rasp-net-monitor_stdout.log 2>&1 &\n' /etc/rc.local
+else
+    echo "rasp-net-monitor appears to be already installed:"
+    echo ""
+    echo "$ grep rasp-net-monitor /etc/rc.local"
+    grep rasp-net-monitor /etc/rc.local
+fi
--- rasp-net-monitor.py
+++ rasp-net-monitor.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# This small script has been created due to a Raspberry Pi that was losing WiFi connection after sometime.
+# Objective is to get some logs about what might have happen and to make a reboot when it's happening.
+
+# TODO #1 Use a decent logger to get 'output to file' / 'timestamp' / 'message level'
+# TODO #2 Use 'iwlist' to get WiFi stats
+
+
 import os  # for the 'system' command
 import time  # Import the sleep function from the time module
 
@@ -8,7 +15,7 @@ PING_HOSTNAME = "www.free.fr"
 DELAY_BEFORE_START = 900  # 15 min = Initial time (to avoid restarting every 5 minutes...)
 DELAY_AFTER_SUCCESS = 300  # 5 min = delay after successful ping (was good => no need to do it again so soon)
 DELAY_AFTER_FAILURE = 60  # delay after FAILED ping
-MAX_FAILURES = 5  # Maximum consecutives failures allowed (in a row)
+MAX_FAILURES = 5  # Maximum consecutive failures allowed (in a row, separated by DELAY_AFTER_FAILURE seconds waiting)
 
 
 def main():
@@ -16,19 +23,22 @@ def main():
     consecutive_failures = 0
     while consecutive_failures < MAX_FAILURES:
         response = 0
-        while (response == 0):
+        while response == 0:
             response = os.system("ping -c 1 -w2 " + PING_HOSTNAME)
             if response == 0:
-                print(PING_HOSTNAME + " is up!")
+                print(PING_HOSTNAME + " is up and accessible!")
                 consecutive_failures = 0
                 time.sleep(DELAY_AFTER_SUCCESS)
             else:
-                print(PING_HOSTNAME + " is down!")
-        
-        consecutive_failures = consecutive_failures + 1
-        time.sleep(DELAY_AFTER_FAILURE)
-        
-    print("It is " + str((DELAY_AFTER_FAILURE * MAX_FAILURES) / 60) + " minutes now that " + PING_HOSTNAME + " does not respond to ping. => restarting!!!")
+                print(PING_HOSTNAME + " is not responding as expected!")
+                os.system("sudo iwlist wlp2s0 scan | grep 'Quality=\\|ESSID:\\|Address:\\|Frequency:'")
+
+    consecutive_failures = consecutive_failures + 1
+    time.sleep(DELAY_AFTER_FAILURE)
+
+    minutes_down = (DELAY_AFTER_FAILURE * MAX_FAILURES) / 60
+    print("It is " + str(minutes_down) + " minutes now that "
+          + PING_HOSTNAME + " does not respond to ping. => restarting!!!")
     os.system("sudo shutdown -r now")
 
 
